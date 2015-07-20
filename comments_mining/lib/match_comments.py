0b9cd30c7d83363e3f7e7dcafd3c624c1856214f
huangjuhua <huangjuhua@ppzuche.com>
2015-07-20 19:08:50 +0800

    keyword matching script

--- comments_mining/lib/match_comments.py
+++ comments_mining/lib/match_comments.py
@@ -36,21 +36,21 @@ car_keywords = {}
 for ind in all_comments.index:
     car_id = all_comments['carID'].iloc[ind]
     comments = string.strip(str(all_comments['comment'].iloc[ind]))
-
     if car_id not in car_keywords.keys():
         car_keywords[car_id] =set()
-    car_keywords[car_id].union(search_keywords(comments,keywords))
+    key_sets = search_keywords(comments,keywords)
+    car_keywords[car_id] = car_keywords[car_id].union(key_sets)
 
 result =[]
 for id in car_keywords:
     counts = len(car_keywords[id])
     result.append([id,counts])
 result.sort(key = lambda key: key[1],reverse = True)
-print 'writing to file'
+print result
 ff = open('tags_count','w')
 for line in result:
     ff.writelines(str(line[0]) + ' ' + str(line[1]) + '\n')
-    ff.close()
+ff.close()
 
 
 
